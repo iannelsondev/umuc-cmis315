bd65e0dac9c41c454e105126795a33046adf1fa3
Ian Nelson <iannelson1@gmail.com>
2014-06-08 13:38:42 -0400

    Updated documentation

--- disc2/discussion2.cpp
+++ disc2/discussion2.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+/**
+ * Class to represent two different selection statements
+ * in discussion #2.
+ *
+ * Ian Nelson
+ * CMIS315
+ * Discussion 2
+ * Jeff H. Sanford
+ */
 class DiscussionTwo {
 	public:
 		DiscussionTwo();
@@ -10,11 +19,24 @@ class DiscussionTwo {
 		string determineTemperature(int);
 };
 
+/**
+ * Default constructor.
+ */
 DiscussionTwo::DiscussionTwo() {};
 
+/**
+ * Default destructor.
+ */
 DiscussionTwo::~DiscussionTwo() {};
 
+/**
+ * Finds the string representation of a month.
+ * 
+ * @param  month  Number of the month
+ * @return        String representation of the month
+ */
 string DiscussionTwo::findMonth (int month) {
+	// Switches the month integer.
 	switch (month) {
 		case 1:
 			return "January"; break;
@@ -40,15 +62,24 @@ string DiscussionTwo::findMonth (int month) {
 			return "November"; break;
 		case 12:
 			return "December"; break;
+		// If nothing else matches, return unknown.
 		default:
 			return "Unknown"; break;
 	}
 };
 
+/**
+ * Determines the average seaonsal temperature based on month
+ * 
+ * @param  month  Number of the month
+ * @return        Seasonal average temperature
+ */
 string DiscussionTwo::determineTemperature (int month) {
+	// If the month is between January and March, or October and December...
 	if ( (month <= 3 && month > 0) || (month >= 10 && month <= 12) ) {
 		return "Cold";
 	}
+	// Otherwise...
 	else {
 		return "Warm";
 	}
@@ -60,12 +91,18 @@ string DiscussionTwo::determineTemperature (int month) {
  * @return Exit code
  */
 int main () {
+	// Instance of the class.
 	DiscussionTwo disc;
 
+	// For each month of the year...
 	for (int i = 1; i <= 12; i++) {
+		// Print the month's name.
 		cout << "The month numbered " << i << " is " << disc.findMonth(i) << endl;
+
+		// Print the seasonal average temperature.
 		cout << "The temperature during the month is " << disc.determineTemperature(i) << endl << endl;
 	}
 
+	// Exit cleanly.
 	return 0;
 };
